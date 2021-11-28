aae6c3a8b4afdf38f1177a33291b937580fec1e5
Zeinab Farag <zeinabfarag454@gmail.com>
2021-11-28 14:13:47 -0500

    add loader

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
 const getQuotes = async () => {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
 
   try {
@@ -19,6 +21,7 @@ const getQuotes = async () => {
 };
 
 const newQuote = () => {
+  loading();
   let index = Math.floor(Math.random() * apiQuotes.length);
   let quote = apiQuotes[index];
   authorText.textContent = quote.author ? `- ${quote.author}` : "Unknown";
@@ -29,11 +32,21 @@ const newQuote = () => {
     quoteText.classList.remove("long-quote");
   }
   quoteText.textContent = quote.text;
+  complete();
 };
 
 const tweetQuote = () => {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
+};
+
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+const complete = () => {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
 };
 
 newQuoteBtn.addEventListener("click", newQuote);
